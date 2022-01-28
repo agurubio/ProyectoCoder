2707f242ddc74c0131259aa973720b98b9ea8caf
Agustin Rubio <agustin.rubio@gmail.com>
2022-01-28 12:45:51 -0300

    add views

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    return HttpResponse('vista inicio')
+
+def curso(request):
+    return HttpResponse('vista curso')
+
+def profesores(request):
+    return HttpResponse('vista profesores')
+
+def estudiantes(request):
+    return HttpResponse('vista estudiantes')
+
+def entregables(request):
+    return HttpResponse('vista entregables')
