cb71f1f73f5607d1e9e35d8163394848bebf49ca
Agustin Rubio <agustin.rubio@gmail.com>
2022-02-02 09:53:00 -0300

    templates vinculados y herencias

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,14 @@
+from sqlite3 import Cursor
 from django.contrib import admin
 
+from AppCoder.models import Curso, Entregable, Estudiante, Profesor
+
 # Register your models here.
+
+admin.site.register(Curso)
+
+admin.site.register(Estudiante)
+
+admin.site.register(Profesor)
+
+admin.site.register(Entregable)
--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,12 +1,12 @@
+from unicodedata import name
 from xml.etree.ElementInclude import include
 from django.urls import path
 from AppCoder import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name="Inicio"),
     path('cursos', views.curso, name="Cursos"),
-    path('profesores', views.profesores),
-    path('estudiantes', views.estudiantes),
-    path('entregables', views.entregables),
-    
+    path('profesores', views.profesores, name="Profesores"),
+    path('estudiantes', views.estudiantes, name="Estudiantes"),
+    path('entregables', views.entregables, name="Entregables"),
 ]
